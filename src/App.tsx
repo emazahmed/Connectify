539c3e6ff67295ecb3b7872902966e143fea2577
Emazuddin Ahmed <emazahmedshayafuddinchy@gmail.com>
2024-09-04 19:54:47 +0600

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/forms/AuthLayout';
+import RootLayout from './_root/pages/RootLayout';
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello Connectify!
-    </h1>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+
   )
 }
 
